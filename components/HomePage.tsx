0e36bb63478b509e0a971e0d655ca814a379c2af
Jon <jlinford84@gmail.com>
2022-11-14 14:17:07 +0000

    added  home screen to the landing page linked to app

---
+++ components/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Text, Image } from 'react-native'
+
+function HomeScreen() {
+    return(
+        <Text>
+            Working!
+        </Text>
+    )
+}
+    
+
+export default HomeScreen
