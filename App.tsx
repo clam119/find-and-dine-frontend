df6092126fe233623538ff9f78ad64240a1088a1
Ellie <101657425+elliehl@users.noreply.github.com>
2022-11-15 15:45:32 +0000

    Update components in app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { View } from "react-native";
-import HomeScreen from "./components/HomePage";
+import BottomNavBar from "./components/BottomNavBar";
+import HomePage from "./components/HomePage";
 
 import styles from "./components/styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <>
+      <HomePage />
+      <NavigationContainer>
+      <BottomNavBar/>
+      </NavigationContainer>
+      </>
   );
 }
