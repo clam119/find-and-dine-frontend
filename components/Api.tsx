cb058cb3b7315a4edada9c5f429e507b2a05d8a5
Jon <jlinford84@gmail.com>
2022-11-15 12:05:30 +0000

    Added list template and styling using test data

---
+++ components/Api.tsx
@@ -0,0 +1,80 @@
+interface DEMO_CONTENT {
+  id: String;
+  title: String;
+  image: String;
+  review: Number;
+}
+const DEMO_CONTENT = [
+  {
+    id: "13811970880719370165",
+    title: "Archie's",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
+    votes: 1,
+  },
+  {
+    id: "15743151851868695548",
+    title: "Döner Summer Manchester",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
+    votes: 2,
+  },
+  {
+    id: "13811970880719370165A",
+    title: "Archie's",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
+    votes: 3,
+  },
+  {
+    id: "15743151851868695548A",
+    title: "Döner Summer Manchester",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
+    votes: 4,
+  },
+  {
+    id: "13811970880719370165B",
+    title: "Archie's",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
+    votes: 5,
+  },
+  {
+    id: "15743151851868695548C",
+    title: "Döner Summer Manchester",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
+    votes: 4.2,
+  },
+  {
+    id: "13811970880719370165D",
+    title: "Archie's",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
+    votes: 3.5,
+  },
+  {
+    id: "15743151851868695548E",
+    title: "Döner Summer Manchester",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
+    votes: 1.6,
+  },
+  {
+    id: "13811970880719370165F",
+    title: "Archie's",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
+    votes: 0.8,
+  },
+  {
+    id: "15743151851868695548G",
+    title: "Döner Summer Manchester",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
+    votes: 0.4,
+  },
+];
+
+export default DEMO_CONTENT;
