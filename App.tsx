e068dba8cb1e1f6d11123658b35d15f47f0fe23e
clam119 <christopher.lam119@gmail.com>
2022-11-16 12:02:31 +0000

    Import ApolloClient and create new instance of the Apollo Client.

--- App.tsx
+++ App.tsx
@@ -2,15 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { View } from "react-native";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./components/HomePage";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: "https://find-and-dine-backend-production.up.railway.app/graphql",
+  cache: new InMemoryCache()
+})
 
 export default function App() {
   return (
     <>
+    <ApolloProvider client={client}>
       <HomePage />
       <NavigationContainer>
       <BottomNavBar/>
       </NavigationContainer>
+    </ApolloProvider>
       </>
   );
 }
