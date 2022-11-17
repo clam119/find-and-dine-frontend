82cefaeb79d579ce5a31d497c984039c3c4f025e
clam119 <christopher.lam119@gmail.com>
2022-11-17 14:23:29 +0000

    Added the Redux provider.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./components/HomePage";
-import MMKVStorage, { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
-import { StorageContext } from "./components/contexts/StorageContext";
-
-const interestedStorage = new MMKVLoader().initialize();
-const notInterestedStorage = new MMKVLoader().initialize();
+import store from './components/redux/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
     <>
-      <StorageContext.Provider value={{ interestedStorage, notInterestedStorage }}>
+      <Provider store={store}>
         <HomePage />
         <NavigationContainer>
         <BottomNavBar/>
         </NavigationContainer>
-      </StorageContext.Provider>    
+      </Provider>    
     </>
   );
 }
