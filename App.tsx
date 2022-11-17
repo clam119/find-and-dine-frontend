090a0193c7803a8713150b8b08ca195399034bbb
clam119 <christopher.lam119@gmail.com>
2022-11-17 10:47:13 +0000

    Installed MMKVStorage and added StorageContext.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./components/HomePage";
+import MMKVStorage, { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
+import { StorageContext } from "./components/contexts/StorageContext";
 
+const interestedStorage = new MMKVLoader().initialize();
+const notInterestedStorage = new MMKVLoader().initialize();
 
 export default function App() {
   return (
     <>
-      <HomePage />
-      <NavigationContainer>
-      <BottomNavBar/>
-      </NavigationContainer>
-      </>
+      <StorageContext.Provider value={{ interestedStorage, notInterestedStorage }}>
+        <HomePage />
+        <NavigationContainer>
+        <BottomNavBar/>
+        </NavigationContainer>
+      </StorageContext.Provider>    
+    </>
   );
 }
