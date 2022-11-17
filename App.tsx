08340e2e56a8cf0860f73c43515018ba68884838
Christopher Lam <42842394+clam119@users.noreply.github.com>
2022-11-17 17:14:16 +0000

    Revert "Set async storage"

--- App.tsx
+++ App.tsx
@@ -1,22 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./components/HomePage";
-import { store, persistor} from './components/redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from 'react-redux';
-import { Text } from "react-native";
+
 
 export default function App() {
   return (
     <>
-      <Provider store={store}>
-      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-        <HomePage />
-        <NavigationContainer>
-        <BottomNavBar/>
-        </NavigationContainer>
-      </PersistGate>
-      </Provider>    
-    </>
+      <HomePage />
+      <NavigationContainer>
+      <BottomNavBar/>
+      </NavigationContainer>
+      </>
   );
 }
--- components/NotInterested.tsx
+++ components/NotInterested.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from "react";
-import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 import ListTemplate from "./ListTemplate";
 import styles from "./styles";
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { store } from './redux/store';
-import {  resetNotInterested  } from "./redux/action";
 import { useNavigation } from "@react-navigation/native";
+
 import DEMO_CONTENT_NI from "./ApiNotInterested";
 
-
-
-const navigation = useNavigation()
 export default function NotInterested () {
 
-    const dispatch = useDispatch();
-
-    const handleResetNotInterested = () => {
-      dispatch(resetNotInterested);
-      const storeStates = (store.getState());
-      console.log(storeStates, '< all state');
-      console.log(storeStates.notInterested);
-    }
+    const navigation = useNavigation()
 
     return (
     <>
-        <Pressable onPress={ handleResetNotInterested } style={styles.button}>
-          <Text style={styles.buttonText}>RESET NOT INTERESTED</Text>
-        </Pressable>
-        
         <Text style={styles.bottomNavBar}>
         </Text>
         <ListTemplate
