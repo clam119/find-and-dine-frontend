ef0ca85b8158b2cec0554006f86ee62247bea956
clam119 <christopher.lam119@gmail.com>
2022-11-17 16:37:31 +0000

    Add Persist Gate for Redux-Persisted storage.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./components/HomePage";
-import store from './components/redux/store';
+import { store, persistor} from './components/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
+import { Text } from "react-native";
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
         <HomePage />
         <NavigationContainer>
         <BottomNavBar/>
         </NavigationContainer>
+      </PersistGate>
       </Provider>    
     </>
   );
