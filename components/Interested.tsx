d170488e25d610fa5b08b6e3873399f6e5583845
clam119 <christopher.lam119@gmail.com>
2022-11-17 16:38:21 +0000

    Cleaned up Interested card and added a placeholder button to reset all interested cards for future reference for the front-enders.

--- components/Interested.tsx
+++ components/Interested.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, Pressable } from "react-native";
 import styles from "./styles";
 import ListTemplate from "./ListTemplate";
-
+import { useDispatch } from 'react-redux';
+import { resetInterested } from "./redux/action";
+import { store } from './redux/store';
 export default function Interested () {
+
+    const dispatch = useDispatch();
+
+    const handleResetInterested = () => {
+        dispatch(resetInterested);
+        const storeStates = (store.getState());
+        console.log(storeStates, '< all state');
+        console.log(storeStates.notInterested);
+      }
+
     return (
     <>
+        <Pressable onPress={ handleResetInterested } style={styles.button}>
+          <Text style={styles.buttonText}>RESET INTERESTED</Text>
+        </Pressable>
+        
         <Text style={styles.bottomNavBar}>
         </Text>
         <ListTemplate/>
