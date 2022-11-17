5217e45d3db70cfed4d0d2f2d2595449646db02c
Jon <jlinford84@gmail.com>
2022-11-17 15:03:49 +0000

    added individual database list navigation to favourites interested and not interested

--- components/NotInterested.tsx
+++ components/NotInterested.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { View, Text } from "react-native";
 import ListTemplate from "./ListTemplate";
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+import DEMO_CONTENT_NI from "./ApiNotInterested";
 
 export default function NotInterested () {
+
+    const navigation = useNavigation()
+
     return (
     <>
         <Text style={styles.bottomNavBar}>
         </Text>
-        <ListTemplate/>
+        <ListTemplate
+        list={DEMO_CONTENT_NI}
+        />
     </>
     )
 }
