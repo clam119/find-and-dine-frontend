56477b450024b75b245c18bc6e52ed2005e30ef7
clam119 <christopher.lam119@gmail.com>
2022-11-18 16:04:54 +0000

    Changed from static mock data to data that's stored by the user.

--- components/ApiInterested.tsx
+++ components/ApiInterested.tsx
@@ -1,81 +1,25 @@
-interface DEMO_CONTENT_I {
+import { store } from './redux/store';
+
+interface INTERESTED_ITEM {
   id: String;
   title: String;
   image: String;
-  review: Number;
+  votes: Number;
 }
-const DEMO_CONTENT_I = [
-  {
-    id: "13811970880719370165",
-    title: "Archie's",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
-    votes: 1,
-  },
-  {
-    id: "15743151851868695548",
-    title: "Döner Summer Manchester",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
-    votes: 2,
-  },
-  {
-    id: "13811970880719370165A",
-    title: "Archie's",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
-    votes: 3,
-  },
-  {
-    id: "15743151851868695548A",
-    title: "Döner Summer Manchester",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
-    votes: 4,
-  },
-  {
-    id: "13811970880719370165B",
-    title: "Archie's",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
-    votes: 5,
-  },
-  {
-    id: "15743151851868695548C",
-    title: "Döner Summer Manchester",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
-    votes: 4.2,
-  },
-  {
-    id: "13811970880719370165D",
-    title: "Archie's",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
-    votes: 3.5,
-  },
-  {
-    id: "15743151851868695548E",
-    title: "Döner Summer Manchester",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
-    votes: 1.6,
-  },
-  {
-    id: "13811970880719370165F",
-    title: "Archie's",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipOU6HCJ8Ii3RERoT0yyOsFGI8Y3Tf_R31VhMOMP=w1920-h1080-k-no",
-    votes: 0.8,
-  },
-  {
-    id: "15743151851868695548G",
-    title: "Döner Summer Manchester",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipO-ige5X_x8mDoQT2L2O1w8jqNatTkPd4DLEXeO=w1920-h1080-k-no",
-    votes: 0.4,
-  },
-];
 
+export default function interestedData(): INTERESTED_ITEM[] {
 
-export default DEMO_CONTENT_I
+  const storeStates = (store.getState());
+  const interestedState = storeStates.interested
+  const interestedRestaurants = interestedState.restaurants
+  const returnedRestaurants = interestedRestaurants.map(({id, title, image, votes}: INTERESTED_ITEM) => {
+    return {
+      id, 
+      title,
+      image,
+      votes
+    }
+  })
+  return returnedRestaurants;
+
+}
