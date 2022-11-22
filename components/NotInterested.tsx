bc65e02dae8cab7bc9c18d8832cd4be3acd62ef8
clam119 <christopher.lam119@gmail.com>
2022-11-22 14:06:29 +0000

    Added functionality so when reset is pressed it moves it back to main state.

--- components/NotInterested.tsx
+++ components/NotInterested.tsx
@@ -1,23 +1,33 @@
-import React, { useEffect }from "react";
+import React, { useEffect, useState }from "react";
 import { View, Text, Pressable } from "react-native";
 import ListTemplate from "./ListTemplate";
 import styles from "./styles";
 import { useDispatch, useSelector } from 'react-redux';
-import { resetNotInterested } from "./redux/action";
+import { addToMain, resetNotInterested } from "./redux/action";
 import { useNavigation } from "@react-navigation/native";
 import NI_CONTENT from "./ApiNotInterested";
 
 export default function NotInterested () {
     
+    interface Restaurant {
+      id: string
+      title: string
+      image: string
+      votes: number
+    }
+
     const navigation = useNavigation()
     const dispatch = useDispatch();
-    const niInitialState = useSelector(notInterestedInitialState => notInterestedInitialState)
+    const globalState = useSelector(globalState => globalState)
+    const [localNiState, setLocalNiState] = useState([]);
 
     useEffect(() => {
        NI_CONTENT();
-    }, [niInitialState])
+       setLocalNiState(globalState.notInterested.restaurants);
+    }, [globalState])
 
-    const handleResetNotInterested = () => {
+    const handleResetNotInterested = (localNiState: Restaurant[]) => {
+      dispatch(addToMain(globalState.notInterested.restaurants))
       dispatch(resetNotInterested);
     }
 
