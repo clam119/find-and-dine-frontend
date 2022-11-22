1444668c54c368dedfc6486a3a267d6104466c27
clam119 <christopher.lam119@gmail.com>
2022-11-22 14:00:43 +0000

    Added the switch case statements for the main reducers.

--- components/redux/reducer.ts
+++ components/redux/reducer.ts
@@ -1,6 +1,7 @@
 import { ADD_NOT_INTERESTED, RESET_NOT_INTERESTED } from "./action";
 import { ADD_INTERESTED, RESET_INTERESTED } from "./action";
-
+import { ADD_FAVOURITE, RESET_FAVOURITE, REMOVE_FAVOURITE } from "./action";
+import { ADD_TO_MAIN, RESET_MAIN } from './action'
 export const notInterestedInitialState = {
     restaurants: []
 }
@@ -8,22 +9,43 @@ export const notInterestedInitialState = {
 export const interestedInitialState = {
     restaurants: []
 }
-export const notInterestedReducer = (state = notInterestedInitialState, action) => {
-    switch (action.type) {
-        case ADD_NOT_INTERESTED: {
+
+export const mainInitialState = {
+    restaurants: []
+}
+
+export const favouriteInitialState = {
+    restaurants: []
+}
+
+export const favouriteReducer = (state = favouriteInitialState, action) => {
+    switch(action.type) {
+
+        case ADD_FAVOURITE: {
             const { restaurant } = action.payload;
             return {
                 ...state,
-                restaurants: [...state.restaurants, restaurant ]
-            };
+                restaurants: [...state.restaurants, restaurant]
+            }
         }
-        case RESET_NOT_INTERESTED: {
-            return { ...notInterestedInitialState };
+
+        case REMOVE_FAVOURITE: {
+            const { restaurant } = action.payload;
+            const filteredRestaurants = [...state.restaurants].filter(restaurant => restaurant.id !== restaurant.id )
+            return {
+                filteredRestaurants
+            }
         }
+
+        case RESET_FAVOURITE: {
+            return {...favouriteInitialState};
+        }
+         
         default: {
             return state;
         }
     }
+    
 }
 
 export const interestedReducer = (state = interestedInitialState, action) => {
@@ -42,3 +64,41 @@ export const interestedReducer = (state = interestedInitialState, action) => {
             return state;
     }
 }
+
+export const mainReducer = (state = mainInitialState, action) => {
+    switch (action.type) {
+        case RESET_MAIN: {
+            return {...mainInitialState};
+        }
+        case ADD_TO_MAIN: {
+            const  restaurantie  = action.payload;
+            return {
+                restaurants: [...restaurantie.restaurants, ...state.restaurants]
+            }
+        }
+        
+        default: {
+            return state;
+        }
+    }
+}
+
+export const notInterestedReducer = (state = notInterestedInitialState, action, main = mainInitialState) => {
+    switch (action.type) {
+        case ADD_NOT_INTERESTED: {
+            const { restaurant } = action.payload;
+            return {
+                ...state,
+                restaurants: [...state.restaurants, restaurant ]
+            };
+        }
+        case RESET_NOT_INTERESTED: {
+            return {...notInterestedInitialState}
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
+
