e32db3d0ca999fd467859799a82edd52dfb27dbb
clam119 <christopher.lam119@gmail.com>
2022-11-22 16:58:54 +0000

    Added functions to remove favourited restaurants

--- components/Favourites.tsx
+++ components/Favourites.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect }from "react";
+import { Pressable, Text } from "react-native";
 import styles from "./styles";
 import ListTemplate from "./ListTemplate";
-import { resetFavourite } from "./redux/action";
-import DEMO_CONTENT_F from "./ApiFavourites";
+import { addToMain, resetFavourite } from "./redux/action";
+import FAVOURITED_CONTENT from "./ApiFavourites";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Favourites() {
+
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const globalState = useSelector(globalState => globalState)
+  const [localIntState, setLocalIntState] = useState([]);
+
+  useEffect(() => {
+    FAVOURITED_CONTENT();
+    setLocalIntState(globalState.interested.restaurants);
+  },[globalState])
+
+  const handleResetFavourited = (localIntState: Restaurant[]) => {
+    dispatch(resetFavourite);
+    console.log(globalState.favourited)
+  }
+
   return (
     <>
+        <Pressable onPress={ handleResetFavourited } style={styles.button}>
+          <Text style={styles.buttonText}>CLEAR</Text>
+        </Pressable>
       <Text style={styles.bottomNavBar}></Text>
-      <ListTemplate list={DEMO_CONTENT_F} />
+      <ListTemplate list={FAVOURITED_CONTENT()} />
     </>
   );
 }
